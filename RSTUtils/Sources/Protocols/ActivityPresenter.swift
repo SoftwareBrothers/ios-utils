d6a0b9c62da693a0e0c93f942a2487aa1e8ac0bc
Piotr Gorzelany <piotr.gorzelany@rst-it.com>
2017-08-30 10:24:44 +0200

    Added activity indicator presenter

---
+++ RSTUtils/Sources/Protocols/ActivityPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityPresenter.swift
+//  Pods
+//
+//  Created by Piotr Gorzelany on 30/08/2017.
+//
+//
+
+import Foundation
+
+public protocol ActivityPresenter {
+    
+    func showActivityIndicator()
+    func hideActivityIndicator()
+}
